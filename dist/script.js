7612d6dbcb51cbb8cac1e55bc8f2c33149b3579e
akihiro0710 <acedia2074@gmail.com>
2019-06-28 15:47:15 +0900

    変更しない部分をscript.jsに移行

---
+++ dist/script.js
@@ -0,0 +1,46 @@
+const chartCanvas = document.getElementById("chart");
+
+const length = 100;
+const type = 'line';
+const labels = Array(length).fill('');
+let dataList = Array(length).fill({x: 0, y: 0, z: 0});
+const options = {
+  scales: {
+    yAxes: [{
+      ticks: {
+        max: 10,
+        min: -10,
+      }
+    }],
+    xAxes: [{
+      gridLines: {
+        display: false
+      }
+    }]
+  },
+  animation: false
+};
+
+const colors = {x: "rgba(255,0,0,1)", y: "rgba(0,255,0,1)", z: "rgba(0,0,255,1)"};
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    x: 0,
+    y: 0,
+    z: 0
+  }
+});
+const updateView = props => Object.entries(props).forEach(([key, value]) => app[key] = value);
+const createDatasets = dataList => ['x', 'y', 'z'].map(label => ({
+  label,
+  data: dataList.map(data => data[label]).slice(-length),
+  borderColor: colors[label],
+  backgroundColor: colors[label],
+  pointRadius: 0
+}));
+const chart = new Chart(chartCanvas, {type, data: {labels, datasets: createDatasets(dataList)}, options});
+const updateChart = dataList => {
+  chart.data.datasets = createDatasets(dataList);
+  chart.update();
+};
