c40524be6718e8fdc85575a4dc430a0a4a7c948b
masaki-n2015 <masaki-n@pu-toyama.ac.jp>
2019-08-05 17:40:51 +0900

    legendあきらめた．abcの描画頻度あげた

--- dist/script.js
+++ dist/script.js
@@ -4,25 +4,9 @@ const length = 100;
 const type = 'line';
 const labels = Array(length).fill('');
 let dataList = Array(length).fill({x: 0, y: 0, z: 0, a: 0, b: 0, c: 0});
-let legendsList = Array(6).fill(false);
+//let legendsList = Array(6).fill(false);
 const range = 15;
 const options = {
-  options: {
-    onClick: function(e, legendItem) {
-      alert("test");	  
-      var index = legendItem.datasetIndex;
-      legendsList[0] = true;	
-      legendsList[index] = !legendsList[index];
-      var ci = this.chart;
-      var meta = ci.getDatasetMeta(index);
-
-      // controller.isDatasetVisibleのコメントを参照して下さい。
-      meta.hidden = meta.hidden === null? !ci.data.datasets[index].hidden : null;
-
-      // データセットを非表示にしました。チャートを再表示してください。
-      ci.update();
-    }	
-  },
   scales: {
     yAxes: [{
       ticks: {
@@ -65,8 +49,8 @@ const createDatasets = dataList => ['x', 'y', 'z', 'a', 'b', 'c'].map(label => (
 const chart = new Chart(chartCanvas, {type, data: {labels, datasets: createDatasets(dataList)}, options});
 const updateChart = () => {
 　chart.data.datasets = createDatasets(dataList);
-  for(var i=0;i<6;i++){
-    chart.getDatasetMeta(i).hidden = legendsList[i];
-  }
+//  for(var i=0;i<6;i++){
+//    chart.getDatasetMeta(i).hidden = legendsList[i];
+//  }
   chart.update();
 };
